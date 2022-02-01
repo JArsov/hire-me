257c9487da15808c69b508806d53eee082a96f8a
jarsov <jordan@upshiftwork.com>
2022-02-01 16:29:06 +0100

    chore: add prettier and re-configure eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,39 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-  ],
-  parser: '@typescript-eslint/parser',
+  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
   parserOptions: {
+    ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module', // Allows for the use of imports
     ecmaFeatures: {
-      jsx: true,
+      jsx: true, // Allows for the parsing of JSX
     },
-    ecmaVersion: 'latest',
-    sourceType: 'module',
   },
-  plugins: [
-    'react',
-    '@typescript-eslint',
+  settings: {
+    react: {
+      version: 'detect', // Tells eslint-plugin-react to automatically detect the version of React to use
+    },
+  },
+  plugins: ['prettier', 'react-hooks'],
+  extends: [
+    'plugin:react/recommended', // Uses the recommended rules from @eslint-plugin-react
+    'plugin:@typescript-eslint/recommended', // Uses the recommended rules from @typescript-eslint/eslint-plugin
+    'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+    'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
   ],
   rules: {
+    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+    '@typescript-eslint/explicit-function-return-type': 'off', // No need to always define the return type of a function
+    '@typescript-eslint/explicit-module-boundary-types': 'off', // No need to always define the return type of a function
+    '@typescript-eslint/no-empty-interface': 'off', // Allows empty interfaces to be defined (might be useful for network)
+    'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
+    'react-hooks/exhaustive-deps': 'warn', // Checks effect dependencies
+    '@typescript-eslint/no-unused-vars': 'warn', // Self-explanatory
+    'no-console': 'warn', // Never use console
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ], // Throw all prettier errors
+    'react/react-in-jsx-scope': 'off', // With React version >= 17, we don't need to import React in every file
   },
 };
